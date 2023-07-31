a13e72f2c2b325386cd837cb19872127239726d3
CaseyR <caseyroeske22@gmail.com>
2023-07-31 12:51:47 -0500

    edited movies list

--- part1/exercises/src/components/MovieList.js
+++ part1/exercises/src/components/MovieList.js
@@ -10,6 +10,8 @@ class MovieList extends React.Component {
          <ol>
             <li className={classes.movieText}>{movies[0]}</li>
             <li className={classes.movieText}>{movies[1]}</li>
+            <li className={classes.movieText}>{movies[2]}</li>
+            <li className={classes.movieText}>{movies[3]}</li>
          </ol>
       </div>
       );
